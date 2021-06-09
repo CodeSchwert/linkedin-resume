38b0d718ccb7610e041b82355199121efd87af48
CodeSchwert <2661899+CodeSchwert@users.noreply.github.com>
2021-06-09 18:53:20 +1200

    Add schema validation option, write resume json to main directory.

--- parseDataExport.js
+++ parseDataExport.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const unzipper = require('unzipper');
+const resumeSchema = require('resume-schema');
 const parseDataFiles = require('./lib/parseDataFiles');
+
+const VALIDATE = false;
 
 const dataExportPath = path.join(__dirname, 'DataExport');
 
@@ -17,11 +20,47 @@ const zipFiles = fs
 // unzip all the data export zip archives into the DataExport directory
 zipFiles.map((zipFile) => {
   const outputDir = zipFile.split('.zip')[0];
-  fs.createReadStream(zipFile).pipe(unzipper.Extract({ path: `DataExport/${outputDir}` }));
+  fs.createReadStream(zipFile).pipe(
+    unzipper.Extract({ path: `DataExport/${outputDir}` })
+  );
   unzippedFolders.push(outputDir);
 });
 
-console.log(unzippedFolders);
+// console.log(unzippedFolders);
 
 // iterate over unpacked folders
-unzippedFolders.map(exportFolder => parseDataFiles(exportFolder));
+unzippedFolders.map((exportFolder) => {
+  const resumeObject = parseDataFiles(exportFolder);
+
+  if (VALIDATE) {
+    resumeSchema.validate(
+      resumeObject,
+      (err, report) => {
+        if (err) {
+          console.error('The resume was invalid:', err);
+          return;
+        }
+        console.log('Resume validated successfully:', report);
+      },
+      (err) => {
+        console.error('The resume was invalid:', err);
+      }
+    );
+  }
+
+  console.log('exportFolder', exportFolder);
+  // console.log(resumeObject);
+
+  /**
+   * Merge overrides here!
+   */
+
+  /**
+   * Output files!
+   */
+  fs.writeFileSync(
+    `${exportFolder}_resume.json`,
+    JSON.stringify(resumeObject, null, 2),
+    { encoding: 'utf8' }
+  );
+});
